133063b32600a87133e602b116e77e6605612bf3
Rohit Rajagopal <70361224+rraj786@users.noreply.github.com>
2024-07-31 21:53:23 +1200

    Removed AnalyseChat class and added function call to generate report

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 '''
-    The following script reads in Messenger chats, analyses messages to generate 
-    statistical summaries, and conducts sentiment analysis to assess the emotional 
+    The following script reads in a Messenger chat, analyses its content to generate 
+    statistical summaries, and conducts sentiment and emotion analysis to assess the
     tone of conversations.
     Author: Rohit Rajagopal
 '''
 
 
 import argparse
-from metrics import *
+from generate_report import *
 import os
 from process_json import *
 
@@ -16,24 +16,16 @@ from process_json import *
 parser = argparse.ArgumentParser()
 curr_dir = os.getcwd()
 parser.add_argument('--dir', type = str, default = os.path.join(curr_dir, 'data'), help = 'Path to directory containing raw Messenger Data')
+parser.add_argument('--batch_size', type = str, default = 48, help = 'Batch size to run Sentiment and Emotion Analysis models')
 parser.add_argument('--save_dir', type = str, default = os.path.join(curr_dir, 'outputs'), help = 'Path to save Messenger chat metrics')
-parser.add_argument('--cumulative_msgs_time_period', type = str, default = 'week', 
-    help = 'Time period to aggregate cumulative messages over time (must be date, week, month, quarter, or year)')
-parser.add_argument('--chat_activity_time_period', type = str, default = 'hour', 
-    help = 'Time period to aggregate chat activity (must be hour, day, week, month, quarter, or year)')
 args = parser.parse_args()
 
 # Parse raw data into dataframe
-chat_history = process_json(args.dir)
+group_name, chat_history = process_json(args.dir)
 
 # Create new directory to save outputs if not available already
 if not os.path.exists(args.save_dir):
     os.makedirs(args.save_dir)
 
-# Generate metrics and save to relevant directory
-metrics = AnalyseChat(chat_history, 64, args.save_dir)
-# metrics.summary_stats()
-# metrics.messages_over_time(args.cumulative_msgs_time_period)
-# metrics.chat_activity(args.chat_activity_time_period)
-# metrics.react_analysis()
-metrics.word_analysis()
+# Generate Streamlit HTML report
+generate_report(args, group_name, chat_history)
